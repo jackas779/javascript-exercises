061ffe4e5fe3c728c03f51a182e7fdf865815412
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-08-26 11:12:39 -0500

    Complete sumAll 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    if(a > b) a = b + (b=a,0)
+    else if( a < 0 || typeof b != 'number' )return 'ERROR';
+    let c=0;
+    for (a; a <= b; a++) {
+        c = c+a;
+    }
+    return c;
 };
 
 // Do not edit below this line

0c64488e8394813e368ad728feb124a895222367
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-08-25 21:17:22 -0500

    complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(a,b) {
 
+    if(b<0){
+        return 'ERROR';
+    }else if(b==0){
+        return "";
+    }
+    if(a==''){
+        return a;
+    }
+    
+    for (let index = 1; index < b; index++) {
+        a = a+'hey';        
+    }
+    return a;
 };
 
 // Do not edit below this line

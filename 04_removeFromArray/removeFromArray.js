48e2ad9908156a9e3b8898f4476325d385c0b0c5
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-08-25 23:40:17 -0500

    Complete revomeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,val,val2,val3,val4) {
+    const result = arr.filter(
+        (arr) => arr !== val && arr != val2 && arr != val3 && arr != val4
+    );
+    return result;
 };
 
 // Do not edit below this line

da3a92c5650da05793eedb835e0e326a9ebb54c0
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-08-27 13:30:59 -0500

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  f = (f -32) * 5/9;
+  return Number(f.toFixed(1)) ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  c = (c * 9/5) + 32;
+  return Number(c.toFixed(1));
 };
 
 // Do not edit below this line

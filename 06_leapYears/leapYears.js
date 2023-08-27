b7c6614a457ff51d15fbafae03d59b601de2684b
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-08-27 00:18:50 -0500

    Complete leapYears 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(a) {
+    let year =  a.toString().slice(-2);
+    if(year  == '00'){
+        if(a % 400 == 0)return true 
+        else return false
+    }
+    if(a % 4 == 0)return true;
+    return false;
 };
 
 // Do not edit below this line

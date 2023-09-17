1c9c701c3f85203dd2499120832d3972bc5138b4
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-09-17 15:33:06 -0500

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b ;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let suma = 0;
+  a.forEach((e) => {suma+=e})
+  return suma;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let multi = 1;
+  a.forEach((e) => {multi*=e})
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let power = 1;
+  for (let i = 0; i < b; i++) {
+    power *= a;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factorial=1;
+  if(a == 0)return 1;
+  for (let i = 1; i <= a; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line

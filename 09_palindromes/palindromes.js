c08577bcee638a89faf2d60ff2faa42ff55a9332
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-09-17 16:29:52 -0500

    Complete palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const patron = /[!,.]/g;
+    let b= a.replace(patron,'').split(/\s+/).join('').toLowerCase();
+    let c =b.split('').reverse().join('').toLowerCase();
+    if(b==c)return true;
+    return false;
 };
 
 // Do not edit below this line

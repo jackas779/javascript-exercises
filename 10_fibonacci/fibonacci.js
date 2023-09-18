740b3300a2a40e1837784e51a63b533ed646bb00
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-09-17 21:52:16 -0500

    complete fibonachi

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let nachi = [0,1];
+    if(a<0)return 'OOPS';
+    for(let b,c,i = 2 ; i <= a ;i++){
+        b = i-1;
+        c= i-2;
+        nachi[i]=nachi[b]+nachi[c];
+    }
+    return nachi[a];
 };
 
 // Do not edit below this line

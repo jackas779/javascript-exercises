3ff7c49238a57ae960d09ae38f7b98bddecc6e30
jackas779 <50506841+jackas779@users.noreply.github.com>
2023-09-19 20:11:37 -0500

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let edad;
+let fecha = new Date().getFullYear();
+people.reduce((ant,acum) => {
+  if(ant.yearOfDeath === undefined) ant.yearOfDeath = fecha
+  if((ant.yearOfDeath - ant.yearOfBirth) < (acum.yearOfDeath - acum.yearOfBirth) ){  
+    edad = acum;
+  }
+  else{
+    edad = ant;
+  }
+  return edad;
+});
+return edad
 };
 
 // Do not edit below this line
